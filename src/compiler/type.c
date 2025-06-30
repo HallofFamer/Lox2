7e785f805ad26a4eb926d320d4b90711dfca458e
HallofFamer <worldoffame@hotmail.com>
2025-06-30 08:48:45 -0400

    Add new function typeTableAddAll(from, to), modify output function for field types.

--- src/compiler/type.c
+++ src/compiler/type.c
@@ -216,6 +216,17 @@ bool typeTableSet(TypeTable* typetab, ObjString* key, TypeInfo* value) {
     return true;
 }
 
+void typeTableAddAll(TypeTable* from, TypeTable* to) {
+    for (int i = 0; i < from->capacity; i++) {
+        TypeEntry* entry = &from->entries[i];
+        if (entry != NULL && entry->key != NULL) {
+            FieldTypeInfo* fromFieldType = AS_FIELD_TYPE(entry->value);
+            FieldTypeInfo* fieldType = newFieldTypeInfo(fromFieldType->baseType.id, entry->key, fromFieldType->declaredType, fromFieldType->isMutable, fromFieldType->hasInitializer);
+            entry->value = (TypeInfo*)fieldType;
+        }
+    }
+}
+
 TypeInfo* typeTableMethodLookup(TypeInfo* type, ObjString* key) {
     if (type == NULL || !IS_BEHAVIOR_TYPE(type)) return NULL;
     BehaviorTypeInfo* behaviorType = AS_BEHAVIOR_TYPE(type);
